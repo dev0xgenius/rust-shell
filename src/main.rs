39f300940e05d28140ef3b03c26766bd7a561ec8
0xgenius <dev0xgenius@gmail.com>
2026-05-21 17:24:47 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,14 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
+    let mut cmd = String::new();
     io::stdout().flush().unwrap();
+
+    match io::stdin().read_line(&mut cmd) {
+        Ok(..) => {
+            let cmd = cmd.trim();
+            println!("{cmd}: command not found");
+        }
+        Err(_) => println!("An internal error occured!"),
+    }
 }
