9a7105e325d0e721a1a63c53bbfd8c1c253e92e2
0xgenius <dev0xgenius@gmail.com>
2026-06-21 10:48:36 +0100

    codecrafters submit [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
-struct Cmd {
-    cmd: String,
-    args: i32,
-    arg_required: bool,
+// pub struct Argument {
+//     pub arg: String,
+//     pub required: bool,
+// }
+
+pub struct Cmd {
+    pub cmd: String,
+    pub description: String,
+    pub operation: Box<dyn Fn()>,
 }
 
 impl Cmd {
     pub fn exec(&self) {
         println!("Executing Command... {}", self.cmd);
+        (self.operation)();
+
         // Do the thing that is expected of the command
         // by passing the arguments
     }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 #[allow(unused_imports)]
-use std::convert::TryInto;
+use codecrafters_shell::Cmd;
+// use std::convert::TryInto;
 use std::io::{self, Write};
 
-const KEYWORDS: [&str; 1] = ["echo"];
+// const KEYWORDS: [&str; 1] = ["echo"];
 
 fn main() {
+    let mut valid_commands: Vec<Cmd> = Vec::new();
+    valid_commands.push(Cmd {
+        cmd: String::from("exit"),
+        description: String::from("exit is a shell built-in"),
+        operation: Box::new(|| std::process::exit(0)),
+    });
+
     loop {
         print!("$ ");
         let mut input = String::new();
@@ -12,7 +20,7 @@ fn main() {
 
         io::stdin()
             .read_line(&mut input)
-            .expect("An internal error occured");
+            .expect("an internal error occured");
 
         let input: Vec<&str> = input
             .split(' ')
@@ -22,19 +30,26 @@ fn main() {
 
         if !input.is_empty() {
             let cmd = &input[0];
-            let args = &input[1..];
+            let _args = &input[1..];
 
-            if *cmd == "exit" {
-                if args.len() > 1 {
-                    println!("{cmd}: too many arguments");
-                } else if args.is_empty() {
-                    std::process::exit(0);
-                } else {
-                    let code = args[0].parse().unwrap_or(0);
-                    std::process::exit(code);
-                }
-            } else if *cmd == "echo" {
+            match valid_commands.iter().find(|&command| command.cmd == *cmd) {
+                Some(cmd) => cmd.exec(),
+                None => println!("{}: not found", *cmd),
             }
+
+            // if *cmd == "exit" {
+            //     if args.len() > 1 {
+            //         println!("{cmd}: too many arguments");
+            //     } else if args.is_empty() {
+            //         std::process::exit(0);
+            //     } else {
+            //         let code = args[0].parse().unwrap_or(0);
+            //         std::process::exit(code);
+            //     }
+            // } else if *cmd == "echo" && !args.is_empty() {
+            //     let output = args.join(" ");
+            //     println!("{output}\n");
+            // }
         }
     }
 }
